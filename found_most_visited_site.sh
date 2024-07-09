f77772dd5e8cefa96014a087ac629afcac48d1a0
jvverde <jvverde@outlook.com>
2024-07-09 23:40:51 +0100

    minor thing

--- found_most_visited_site.sh
+++ found_most_visited_site.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sqlite3 dbs/ebird-"${1?"Usage: $0 location"}".db "SELECT COUNT(*) as freq, location, map_url FROM sightings WHERE date >= date('now', '-${2:-30} days') group by location order by freq desc"
+sqlite3 dbs/ebird-"${1?"Usage: $0 location"}".db "SELECT COUNT(*) as freq, location, map FROM sightings WHERE date >= date('now', '-${2:-30} days') group by location order by freq desc"
