3b5086955b4faa8f0461500a45d17ad90ea78dc7
jvverde <jvverde@outlook.com>
2024-07-09 15:27:40 +0100

    import all email files

---
+++ mail2db_all.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Treat unset variables as an error and exit immediately
+set -u
+
+# Function to display usage information
+usage() {
+    echo "Usage: $0 [-d dbsdir] [-e errdir] datadirs..."
+    exit 1
+}
+
+# Default values
+declare dbsdir="dbs"
+declare errdir="error_logs"
+
+# Parse options
+while getopts "d:e:" opt; do
+    case "$opt" in
+        d) dbsdir="$OPTARG" ;;
+        e) errdir="$OPTARG" ;;
+        *) usage ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+# Ensure error and dbs directories exists
+mkdir -p "$errdir"
+mkdir -p "$dbsdir"
+
+# Declare an array for directories and use parameter expansion to set the default directory to current directory if no arguments are provided
+declare -a dirs=("${@:-.}")
+
+# Use find directly over the array of directories
+find "${dirs[@]}" -type f -name '*.eml' -printf '%h\n' | sort -u | while read -r dir
+do
+    # Extract the directory name
+    declare dirname="$(basename "$dir")"
+    # Run the command
+    ./mail2db.pl --db "${dbsdir}/ebird-${dirname}.db" "$dir"/* 2>"$errdir/${dirname}.lst"
+done
